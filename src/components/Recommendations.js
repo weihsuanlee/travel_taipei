6aa624f0ff041d41200c7a714fbb3186890c3fdd
Wei Lee <weihsuan006@gmail.com>
2021-03-31 23:42:58 +0800

    周邊景點刪減為4個

--- src/components/Recommendations.js
+++ src/components/Recommendations.js
@@ -1,6 +1,10 @@
 import { useEffect, useState } from 'react'
 import axios from '../api/axios'
 import requests from '../api/requests'
+import './Recommendations.scss'
+import { withRouter } from 'react-router-dom'
+import LocationOnRoundedIcon from '@material-ui/icons/LocationOnRounded'
+
 function Recommendations(props) {
   const { attraction } = props
   const [recommendations, setRecommendations] = useState([])
@@ -12,18 +16,47 @@ function Recommendations(props) {
           elong: attraction.elong,
         },
       })
-      setRecommendations(response.data.data.slice(0, 7))
+      // console.log(response.data.data)
+      setRecommendations(response.data.data.slice(1, 5))
       return response
     }
     fetchData()
   }, [attraction])
   return (
-    <div>
-      {recommendations.map((recommendation) => (
-        <p key={recommendation.id}>{recommendation.name}</p>
-      ))}
-    </div>
+    <>
+      {recommendations.length > 0 && (
+        <div className="recommendations">
+          <div className="recommendation-title">週邊景點</div>
+          {recommendations.map((recommendation) => (
+            <div
+              key={recommendation.id}
+              className="recommendation"
+              onClick={() => {
+                props.history.push('/' + recommendation.id)
+              }}
+            >
+              <img
+                className="recommendation-image"
+                src={
+                  recommendation.images[0]
+                    ? recommendation.images[0].src
+                    : '../../images/notfound.jpeg'
+                }
+                alt=""
+              />
+              <div className="recommendation-align">
+                <p>{recommendation.name}</p>
+                <p className="recommendation-address">
+                  <LocationOnRoundedIcon />
+                  <span>{recommendation.address}</span>
+                </p>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
+    </>
   )
 }
 
-export default Recommendations
+export default withRouter(Recommendations)
