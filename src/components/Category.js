c8a11f839be2f597b85bd378492c1246466f3b1d
Wei Lee <weihsuan006@gmail.com>
2021-04-01 22:50:20 +0800

    category active

--- src/components/Category.js
+++ src/components/Category.js
@@ -5,10 +5,15 @@ import Tab from '@material-ui/core/Tab'
 import { useDispatch } from 'react-redux'
 import { changeCateoryId, changePageNow } from '../features/attractionsSlice'
 import './Category.scss'
+import { selectCategory } from '../features/attractionsSlice'
+import { useSelector } from 'react-redux'
 
 function Category() {
   const dispatch = useDispatch()
-  const [value, setValue] = useState(0)
+  const categoryId = useSelector(selectCategory)
+
+  const [value, setValue] = useState(categoryId)
+
   const handleChange = (event, newValue) => {
     setValue(newValue)
   }
@@ -22,6 +27,7 @@ function Category() {
       onClick={() => {
         handleClick(categoryId)
       }}
+      value={categoryId}
     />
   )
   return (
@@ -39,14 +45,17 @@ function Category() {
           {TabItem('全部景點', 0)}
           {TabItem('藝文場館', 15)}
           {TabItem('戶外踏青', 16)}
+          {TabItem('公共藝術', 18)}
           {TabItem('親子共遊', 19)}
           {TabItem('歷史建築', 13)}
           {TabItem('夜市商圈', 23)}
+          {TabItem('單車遊蹤', 12)}
           {TabItem('主題商街', 24)}
           {TabItem('宗教信仰', 14)}
+          {TabItem('養生溫泉', 11)}
+          {TabItem('藍色水路', 17)}
           {TabItem('無障礙旅遊', 25)}
           {TabItem('北北基景點', 20)}
-          {TabItem('單車遊蹤', 12)}
         </Tabs>
       </AppBar>
     </div>
