5648c6c1984f12c7fb50ceb9ae1e6c3b18c0d2ad
Wei Lee <weihsuan006@gmail.com>
2021-04-01 16:00:29 +0800

    收合展開詳細資訊

--- src/components/DetailInfo.js
+++ src/components/DetailInfo.js
@@ -1,4 +1,6 @@
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
 import './DetailInfo.scss'
+
 function DetailInfo(props) {
   const { attraction } = props
   return (
@@ -8,7 +10,15 @@ function DetailInfo(props) {
           <>
             <div className="detail-info-title">詳細資訊</div>
             <div className="detail-intro">
-              <p>{attraction.introduction}</p>
+              <input type="checkbox" id="expanded" />
+              <p className="collapsed-intro">{attraction.introduction}</p>
+              <label
+                htmlFor="expanded"
+                role="button"
+                className="collapsed-toggle"
+              >
+                <ExpandMoreIcon />
+              </label>
             </div>
           </>
         )}
