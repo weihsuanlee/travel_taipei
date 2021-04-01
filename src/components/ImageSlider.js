ea247d9e42f663bfa97896dd053e2807969bce49
Wei Lee <weihsuan006@gmail.com>
2021-04-02 00:03:25 +0800

    slider rerender 1st slide

--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -2,8 +2,10 @@ import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
 import Slider from 'react-slick'
 import './ImageSlider.scss'
+import { useRef, useEffect } from 'react'
 
 function ImageSlider(props) {
+  const sliderRef = useRef()
   const { images } = props
   const settings = {
     arrows: false,
@@ -13,11 +15,14 @@ function ImageSlider(props) {
     slidesToShow: 1,
     slidesToScroll: 1,
   }
-  // console.log(images)
+  console.log(sliderRef.current)
+  useEffect(() => {
+    sliderRef.current.slickGoTo(0)
+  }, [images])
   return (
     <div className="detail-slider-wrapper">
       {images?.length > 0 && (
-        <Slider {...settings} className="detail-slider">
+        <Slider {...settings} className="detail-slider" ref={sliderRef}>
           {images.map((v, i) => (
             <div className="detail-slider-image" key={i}>
               <img src={v.src} alt="" />
